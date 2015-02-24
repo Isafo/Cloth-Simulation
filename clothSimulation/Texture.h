411a3467aa80777640fff96c6937bc3c34f7284d
isafo <isabelle.forsman@gmail.com>
2015-02-24 19:03:31 +0100

    removed texture files

--- clothSimulation/Texture.h
+++
@@ -1,7 +0,0 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <stdio.h>
-
-GLuint loadBMP(const char * imagePath);
-
-void texture(unsigned int width, unsigned int height, unsigned char *data);
