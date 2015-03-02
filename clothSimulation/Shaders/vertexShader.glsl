0895077cecf17f7c286755ca7ad705ff2f78a2f0
isafo <isabelle.forsman@gmail.com>
2015-03-02 15:57:12 +0100

    calculating normals and blinn-shading, many buggs

--- clothSimulation/Shaders/vertexShader.glsl
+++ clothSimulation/Shaders/vertexShader.glsl
@@ -1,12 +1,22 @@
-# version 330
+# version 450
 
-
+attribute vec3 normals;
 attribute vec3 coord3d;
 attribute vec3 v_color;
 varying vec3 f_color; // for sending color from vertexshader to fragmentshader
-uniform mat4 mvp; //  global transformation matrix
+varying vec3 vertPos;
+uniform mat4 mv;
+uniform mat4 p;
+
+out vec3 interpolatedNormal;
+
 
 void main(void) {
-	gl_Position = mvp * vec4(coord3d, 1.0);
-	f_color = v_color; // send the color to the fragmentshader
+
+	interpolatedNormal = mat3(mv) * normals;
+	gl_Position = mv * p * vec4(coord3d, 1.0);
+	f_color = v_color;
+
+	vec4 vertPos4 = p * vec4(coord3d, 1.0);
+	vertPos = vec3(vertPos4) / vertPos4.w;
 }
--- clothSimulation/main.cpp
+++ clothSimulation/main.cpp
@@ -18,14 +18,15 @@ using namespace glm;
 static void key_callback (GLFWwindow* window, int key, int scancode, int action, int mods);
 static void error_callback (int error, const char* description);
 void drawTriangles(vector<glm::vec3> particles, Shader phongShader);
+void calculateVerticeNormal(vector <vec3> drawOrder, vector <vec3> surfaceNormals, vector <vec3> &verticeNormals);
 
 //-----------------------
 // variable declarations 
 //-----------------------
 
-GLuint vbo_triangle, vbo_triangle_colors; // Vertex Buffer Objects, for storing vertices directly in the graphics card
-GLint attribute_coord3d, attribute_v_color;
-GLint uniform_mvp;
+GLuint vbo_triangle, vbo_triangle_colors, vbo_vertices_normals; // Vertex Buffer Objects, for storing vertices directly in the graphics card
+GLint attribute_coord3d, attribute_v_color, attribute_vertices_normals;
+GLint uniform_mv, uniform_p;
 
 
 struct attributes {
@@ -107,9 +108,21 @@ int main(void) {
 		return 0;
 	}
 
-	uniform_mvp = glGetUniformLocation(phongShader.programID, "mvp");
-	if (uniform_mvp == -1) {
-		fprintf(stderr, "Could not bind uniform %s\n", "mvp");
+	uniform_mv = glGetUniformLocation(phongShader.programID, "mv");
+	if (uniform_mv == -1) {
+		fprintf(stderr, "Could not bind uniform %s\n", "mv");
+		return 0;
+	}
+
+	uniform_p = glGetUniformLocation(phongShader.programID, "p");
+	if (uniform_p == -1) {
+		fprintf(stderr, "Could not bind uniform %s\n", "p");
+		return 0;
+	}
+
+	attribute_vertices_normals = glGetAttribLocation(phongShader.programID, "normals");
+	if (attribute_vertices_normals == -1) {
+		fprintf(stderr, "Could not bind uniform %s\n", "normals");
 		return 0;
 	}
 
@@ -163,20 +176,26 @@ void drawTriangles(vector<glm::vec3> particles, Shader phongShader) {
 	mat4 view = glm::lookAt(vec3(0.3f, 0.0f, -2.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f)); // get the view matrix
 
 	vector<glm::vec3> drawOrder = MakeTriangles(particles); // orders input so that the normal points in the correct direction
-	//const int vectorSize = drawOrder.size() * 3;
+
 	const int vectorSize = ((nrOfParticlesHorizontally -1 )*(nrOfParticlesVertically - 1) )*6*3;
-	//cout << drawOrder.size() * 3 << endl;
-	//cout << vectorSize << endl;
 	GLfloat clothVertices[vectorSize]; // contains the cloth vertices coordiates
 	GLushort clothElements[vectorSize]; // contains the order the particles should be drawn in
 	GLfloat clothColors[vectorSize]; // contains the vertices colors
+	GLfloat normals[vectorSize/3];
 
 	// insert the coordinates, elements and colors in arrays.
 	// i is used for taking 3 particles each loop, 3 particles -> i = i + 3
 	// while j is used to save the 3 particles x, y and z coordinates and color in the arrays, 9 coordinates -> j = j + 9
 	// loop continues untill there is less than 3 particles left in drawOrder
-
 	int row = 0;
+	vec3 P1 = vec3(0.0f);
+	vec3 P2 = vec3(0.0f);
+	vec3 P3 = vec3(0.0f);
+	vec3 V1 = vec3(0.0f);
+	vec3 V2 = vec3(0.0f);
+	vector <vec3> surfaceNormals;
+	vector <vec3> verticeNormals;
+
 	for (int i = 0, j = 0; i < drawOrder.size(); i = i + 3, j = j + 9) {
 
 		if (i % 2 == 0) {
@@ -249,14 +268,33 @@ void drawTriangles(vector<glm::vec3> particles, Shader phongShader) {
 				clothColors[j + 8] = 1.0f;
 
 			}
+
+			//calculate the surface normal
+			P1 = vec3(drawOrder[i].x, drawOrder[i].y, drawOrder[i].z);
+			P2 = vec3(drawOrder[i + 1].x, drawOrder[i + 1].y, drawOrder[i + 1].z);
+			P3 = vec3(drawOrder[i + 2].x, drawOrder[i + 2].y, drawOrder[i + 2].z);
+
+			V1 = P2 - P1;
+			V2 = P3 - P1;
+
+			surfaceNormals.push_back(cross(V2, V1)); // calculate surface normal
 		}
 	}
 
+	calculateVerticeNormal(drawOrder, surfaceNormals, verticeNormals);
+
+	for (int i = 0; i < verticeNormals.size(); i++){
+		normals[i] = verticeNormals[i].x;
+		normals[i] = verticeNormals[i].y;
+		normals[i] = verticeNormals[i].z;
+	}
 
 	// generate and bind buffer for clothVertices
 	glGenBuffers(1, &vbo_cloth_vertices);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_cloth_vertices);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(clothVertices), clothVertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
 
 	// generate and bind buffer for clothElements
 	glGenBuffers(1, &ibo_cloth_elements);
@@ -268,12 +306,18 @@ void drawTriangles(vector<glm::vec3> particles, Shader phongShader) {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_cloth_colors);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(clothColors), clothColors, GL_STATIC_DRAW);
 
+	// generate and bind buffer for verticeNormals
+	glGenBuffers(1, &vbo_vertices_normals);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices_normals);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(normals), normals, GL_STATIC_DRAW);
 
 	glUseProgram(phongShader.programID);
 
 	// calculate the global transform matrix
-	glm::mat4 mvp = frustum * view;
-	glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
+	glUniformMatrix4fv(uniform_p, 1, GL_FALSE, glm::value_ptr(frustum));
+
+	glm::mat4 mv = view;
+	glUniformMatrix4fv(uniform_mv, 1, GL_FALSE, glm::value_ptr(mv));
 
 	//bind cloth coordinates
 	glEnableVertexAttribArray(attribute_coord3d);
@@ -284,6 +328,11 @@ void drawTriangles(vector<glm::vec3> particles, Shader phongShader) {
 	glEnableVertexAttribArray(attribute_v_color);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_cloth_colors);
 	glVertexAttribPointer(attribute_v_color, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	// bind normals
+	glEnableVertexAttribArray(attribute_vertices_normals);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices_normals);
+	glVertexAttribPointer(attribute_vertices_normals, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	// bind buffer and draw
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_cloth_elements);
@@ -296,4 +345,87 @@ void drawTriangles(vector<glm::vec3> particles, Shader phongShader) {
 	glDisableVertexAttribArray(attribute_v_color);
 	glDeleteBuffers(1, &ibo_cloth_elements);
 
+}
+
+//calculate the vertices normals from the surface normals
+void calculateVerticeNormal(vector <vec3> drawOrder, vector <vec3> surfaceNormals, vector <vec3> &verticeNormals) {
+	vec3 tempNormal;
+	int row = 1;
+	bool firstParticle = true;
+	for (int i = 0; i < drawOrder.size() / 3; i++) { // TODO: i måste följa antalet partiklar inte antalet surfacenormals!!
+		if (row == 1 || row == nrOfParticlesVertically) { // the first row of particles
+			if (i != 0 && i % (nrOfParticlesHorizontally * row) == 0 && row == 0) { // last particle on the first row
+				tempNormal = vec3(surfaceNormals[(i - 1) * 2].x + surfaceNormals[(i - 1) * 2 - 1].x,
+					surfaceNormals[(i - 1) * 2].y + surfaceNormals[(i - 1) * 2 - 1].y,
+					surfaceNormals[(i - 1) * 2].z + surfaceNormals[(i - 1) * 2 - 1].z);
+				row++;
+				firstParticle = true;
+			}
+			else if (firstParticle == true) { // first particle on the first row
+				tempNormal = vec3(surfaceNormals[i].x,
+					surfaceNormals[i].y,
+					surfaceNormals[i].z);
+				firstParticle = false;
+
+			}
+			else{ //middle particle on first row 
+				//tempNormal = vec3(surfaceNormals[(i - 1) * 2].x + surfaceNormals[(i - 1) * 2 - 1].x + surfaceNormals[i * 2 - 1].x,
+				//				  surfaceNormals[(i - 1) * 2].y + surfaceNormals[(i - 1) * 2 - 1].y + surfaceNormals[i * 2 - 1].y,
+				//				  surfaceNormals[(i - 1) * 2].z + surfaceNormals[(i - 1) * 2 - 1].z + surfaceNormals[i * 2 - 1].z);
+			}
+		}
+		else if (row == nrOfParticlesVertically) { // last row of particles
+
+			if (row == nrOfParticlesVertically && i / (nrOfParticlesHorizontally * row) == 1) { // last particle in the cloth is only connected by 1 triangle
+				tempNormal = vec3(surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].x,
+					surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].y,
+					surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].z);
+				firstParticle = true;
+			}
+			else if (firstParticle == true) { // first particle
+				tempNormal = vec3(surfaceNormals[(i - nrOfParticlesHorizontally) * 2].x + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].x,
+					surfaceNormals[(i - nrOfParticlesHorizontally) * 2].y + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].y,
+					surfaceNormals[(i - nrOfParticlesHorizontally) * 2].z + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].z);
+				firstParticle = false;
+			}
+			else { // middle particle
+				tempNormal = vec3(surfaceNormals[(i - nrOfParticlesHorizontally) * 2].x + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].x
+					+ surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].x,
+					surfaceNormals[(i - nrOfParticlesHorizontally) * 2].y + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].y
+					+ surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].y,
+					surfaceNormals[(i - nrOfParticlesHorizontally) * 2].z + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].z
+					+ surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].z);
+			}
+
+		}
+		else { // a middle row
+			if (firstParticle == true) { // first particle on a middle row, affected by 3 triangles, two from the row above and the first one on its row
+				tempNormal = vec3(surfaceNormals[(i - nrOfParticlesHorizontally) * 2].x + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].x + surfaceNormals[i * 2 - 1].x,
+					surfaceNormals[(i - nrOfParticlesHorizontally) * 2].y + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].y + surfaceNormals[i * 2 - 1].y,
+					surfaceNormals[(i - nrOfParticlesHorizontally) * 2].z + surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].z + surfaceNormals[i * 2 - 1].z);
+				firstParticle = false;
+			}
+			else if (i / (nrOfParticlesHorizontally * row) == 1) { // last particle on a middle row
+				tempNormal = vec3(surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].x, //+ surfaceNormals[(i - 1) * 2].x + surfaceNormals[(i - 1) * 2 - 1].x,
+					surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].y, //+ surfaceNormals[(i - 1) * 2].y + surfaceNormals[(i - 1) * 2 - 1].y,
+					surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].z); // + surfaceNormals[(i - 1) * 2].z + surfaceNormals[(i - 1) * 2 - 1].z);
+				firstParticle = true;
+
+			} else { // middle particle middle row
+				tempNormal = vec3(surfaceNormals[i].x + surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].x + surfaceNormals[(i - nrOfParticlesHorizontally) * 2].x
+					+ surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].x + surfaceNormals[(i - 1) * 2].x + surfaceNormals[(i - 1) * 2 - 1].x
+					+ surfaceNormals[i * 2 - 1].x,
+					surfaceNormals[i].y + surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].y + surfaceNormals[(i - nrOfParticlesHorizontally) * 2].y
+					+ surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].y + surfaceNormals[(i - 1) * 2].y + surfaceNormals[(i - 1) * 2 - 1].y
+					+ surfaceNormals[i * 2 - 1].y,
+					surfaceNormals[i].z + surfaceNormals[(i - nrOfParticlesHorizontally - 1) * 2].z + surfaceNormals[(i - nrOfParticlesHorizontally) * 2].z
+					+ surfaceNormals[(i - nrOfParticlesHorizontally) * 2 - 1].z + surfaceNormals[(i - 1) * 2].z + surfaceNormals[(i - 1) * 2 - 1].z
+					+ surfaceNormals[i * 2 - 1].z);
+			}
+		}
+
+		tempNormal = normalize(tempNormal);
+
+		verticeNormals.push_back(tempNormal);
+	}
 }
