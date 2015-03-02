2825d1ac2fae69ed217eed5ab5093c5c69ec92df
isafo <isabelle.forsman@gmail.com>
2015-03-02 16:51:10 +0100

    temporary working shaders

--- clothSimulation/Shaders/fragmentShader.glsl
+++ clothSimulation/Shaders/fragmentShader.glsl
@@ -1,31 +1,7 @@
-# version 450
+# version 330
 
-in vec3 interpolatedNormal;
-varying vec3 vertPos;
 varying vec3 f_color;
 
-const vec3 lightPos = vec3(1.0f, 1.0f, 1.0f);
-
 void main(void) {
-	float shine = 50;
-	float dLight;
-
-	vec3 lightDir = normalize(lightPos - vertPos);
-	vec3 viewDir = normalize(-vertPos);
-
-	dLight = dot(normalize(lightDir), normalize(interpolatedNormal));
-
-	dLight = max(dLight, 0.0);
-	dLight = min(dLight, 1.0);
-
-	vec3 halfAngle = normalize(lightDir + viewDir);
-	float blinn = dot(normalize(interpolatedNormal), halfAngle);
-
-	blinn = max(blinn, 0);
-
-	blinn = dLight != 0.0 ? blinn : 0.0;
-	blinn = pow(blinn, shine);
-
-	vec3 color = 0.5f*f_color*dLight + f_color*blinn;
-	gl_FragColor = vec4(color, 1.0f);
+	gl_FragColor = vec4(f_color.x, f_color.y, f_color.z, 1.0f);
 }
